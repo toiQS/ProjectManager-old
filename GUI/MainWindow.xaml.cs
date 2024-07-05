e477bd01875cf2f1f46bbf53b2fca25e0a8a70d5
toiQS <88361704+toiQS@users.noreply.github.com>
2024-07-05 15:30:12 +0700

    Update MainWindow.xaml.cs

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace GUI
 {
@@ -21,43 +12,62 @@ namespace GUI
             InitializeComponent();
         }
 
+        // Navigate to the Project Window
         private void ProjectButton_Click(object sender, RoutedEventArgs e)
         {
-            Hide();
-            ProjectWindow projectWindow = new ProjectWindow();
-            projectWindow.Show();
+            ShowWindow<ProjectWindow>();
         }
 
+        // Navigate to the Role Window
         private void RoleButton_Click(object sender, RoutedEventArgs e)
         {
-            Hide();
-            RoleWindow roleWindow = new RoleWindow();
-            roleWindow.Show();
+            ShowWindow<RoleWindow>();
         }
 
+        // Navigate to the Status Window
         private void StatusButton_Click(object sender, RoutedEventArgs e)
         {
-            Hide();
-            StatusWindow statusWindow = new StatusWindow();
-            statusWindow.Show();
+            ShowWindow<StatusWindow>();
         }
 
-        private void ProfileButton_Click(object sender, RoutedEventArgs e)
-        {
-            ViewProfileWindow profileWindow = new ViewProfileWindow();
-            profileWindow.ShowDialog();
-        }
-
-        private void LogoutButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+        // Navigate to the Task Level Window
         private void TaskLevelButton_Click(object sender, RoutedEventArgs e)
         {
-            Hide();
-            TaskLevelsWindow taskLevelsWindow = new TaskLevelsWindow(); 
-            taskLevelsWindow.Show();
+            ShowWindow<TaskLevelsWindow>();
+        }
+
+        // Handle logout button click
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muốn đăng xuất?", "Đăng xuất", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        // Generic method to show a window
+        private void ShowWindow<T>() where T : Window, new()
+        {
+            try
+            {
+                var newWindow = new T
+                {
+                    Owner = this
+                };
+                newWindow.Show();
+                newWindow.Closed += (s, args) => ShowMainWindow();
+                Hide();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error opening window: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        // Show the Main Window
+        private void ShowMainWindow()
+        {
+            Show();
         }
     }
 }
