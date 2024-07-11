14f99a413bda0342a5a28c9a86ddaf9301a5c904
toiQS <88361704+toiQS@users.noreply.github.com>
2024-07-11 14:00:33 +0700

    Update TaskInProjectServices.cs

--- Services/_services/TaskInProjectServices.cs
+++ Services/_services/TaskInProjectServices.cs
@@ -18,6 +18,12 @@ namespace Services._services
         {
             var data = _context.Task_In_Projects.ToList();
             return data;
+        }public IEnumerable<Task_In_Project> GetTasksInProject(int projectId)
+        {
+            var data = _context.Task_In_Projects
+                .Where(x => x.ProjectID == projectId)
+                .ToList();
+            return data;
         }
 
         public Task_In_Project GetTask(int id)
@@ -36,14 +42,14 @@ namespace Services._services
             return 0;
         }
 
-        public bool AddTask(string taskName, string taskDescription, int projectID, DateTime createAt, DateTime startAt, DateTime endAt, int taskLevelID)
+        public bool AddTask(string taskName, string taskDescription, int projectID, DateTime startAt, DateTime endAt, int taskLevelID)
         {
             var task = new Task_In_Project()
             {
                 TaskName = taskName,
                 TaskDescription = taskDescription,
                 ProjectID = projectID,
-                CreateAt = createAt,
+                CreateAt = DateTime.Now,
                 StartAt = startAt,
                 EndAt = endAt,
                 TaskLevelID = taskLevelID
