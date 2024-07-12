515dd1a8a510bce9a32375af96813be4b0b5ae38
toiQS <88361704+toiQS@users.noreply.github.com>
2024-07-12 10:57:49 +0700

    Create OptionWindow.xaml.cs

---
+++ GUI/Option_Form/OptionWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI.Option_Form
+{
+    /// <summary>
+    /// Interaction logic for OptionWindow.xaml
+    /// </summary>
+    public partial class OptionWindow : Window
+    {
+        private readonly int _userId;
+        public OptionWindow(int userId)
+        {
+            InitializeComponent();
+            _userId = userId;
+        }
+
+        private void ManageTaskLevels_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ManageRoles_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ManageStatus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Profile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AboutUs_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
